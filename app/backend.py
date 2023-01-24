8cef7233d957cd42ed650dbf5ee20730d8e861d1
Egor <777egorkalinin777@gmail.com>
2023-01-25 00:38:27 +0300

    zz

--- app/backend.py
+++ app/backend.py
@@ -1,1 +1,17 @@
-from 
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/frontend/mainpage", StaticFiles(directory="frontend/mainpage"), name="mainpage")
+
+
+templates = Jinja2Templates(directory="frontend/mainpage")
+
+
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "xx.html", {"request": request})
