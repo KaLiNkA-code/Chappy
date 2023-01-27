541d0cf79c1aec2340dd0a9195db045978653455
Egor <777egorkalinin777@gmail.com>
2023-01-27 19:00:42 +0300

    tries

--- app/backend.py
+++ app/backend.py
@@ -3,9 +3,11 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+
 app = FastAPI()
 
-app.mount("/frontend/mainpage", StaticFiles(directory="frontend/mainpage"), name="mainpage")
+
+app.mount("/mainpage", StaticFiles(directory="frontend/mainpage"), name="mainpage")
 
 
 templates = Jinja2Templates(directory="frontend/mainpage")
@@ -14,4 +16,9 @@ templates = Jinja2Templates(directory="frontend/mainpage")
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse(
-        "xx.html", {"request": request})
+        "main.html", {"request": request})
+
+
+# @app.post("/nicepage.css")
+# async def nacepage_func(request: Request):
+#     return {}
