ee87cc8e6810e73adeb7d88574990d0e298a420e
kalinka-code <777egorkalinin777@gmail.com>
2023-10-12 00:46:20 +0300

    end creating test multy-proc

--- backend/chappy_backend/main.py
+++ backend/chappy_backend/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI, Request
+import uvicorn
+from enum import Enum
+
+
+class ModelName(str, Enum):
+    chat_id = 'alexnet'
+    photo = 'resnet'
+    text = 'lenet'
+    post_data = 'lenet'
+
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root(request: Request):
     return {"status": "ok", "message": "Hello World from backend"}
+
+
+@app.get("/users/{user_chat_id}/posting")
+async def get_model(user_chat_id: int):
+    return {"user_chat_id": user_chat_id, "message": "Have some residuals"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=5051, reload=True, workers=3)
--- frontend/app.py
+++ frontend/app.py
@@ -2,32 +2,39 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+import uvicorn
 import httpx
 import requests
+
+
 app = FastAPI()
 
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def root(request: Request):
-    response =  templates.TemplateResponse(
-        "main.html", {"request": request})
-    print(type(response))
-    return response
+# @app.get("/")
+# async def root(request: Request):
+#     response =  templates.TemplateResponse(
+#         "main.html", {"request": request})
+#     print(type(response))
+#     return response
 
 
 @app.get("/api")
 async def root_api(request: Request):
-    response = requests.get("http://localhost:5050/")
+    response = requests.get("http://localhost:5051/")
+    print(response)
+    data = response.json()
+    return {"status": "ok", "message": "Hello World from frontend", "data": data}
+
+@app.get("/post/{user_chat_id}")
+async def root_api(request: Request, user_chat_id: int):
+    response = requests.get(f"http://localhost:5051/users/{user_chat_id}/posting")
     print(response)
     data = response.json()
     return {"status": "ok", "message": "Hello World from frontend", "data": data}
 
 
-
-# @app.post("/nicepage.css")
-# async def nacepage_func(request: Request):
-#     return {}
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="localhost", port=5050, reload=True, workers=3)
